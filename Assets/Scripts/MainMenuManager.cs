fcb854d347cd7bd78bc206b6d7c0c510d4e228e2
MartinWachira <67173409+mmwachira@users.noreply.github.com>
2025-12-08 20:44:21 -0500

    feat: integrated main menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    private GameManager gameManager;
+
+    [Header("Game References")]
+    public ItemSpawner itemSpawner;
+    public WeekTimer weekTimer;
+
+    [Header("Menu Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject playPanel;
     public GameObject howToPanel;
     public GameObject optionsPanel;
     public GameObject creditsPanel;
 
+    void Start()
+    {
+        gameManager = FindFirstObjectByType<GameManager>();
+
+        // Show menu, hide game when app starts
+        mainMenuPanel.SetActive(true);
+        playPanel.SetActive(false);
+        howToPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+
+        // Prevent spawner from running at start
+        itemSpawner.StopSpawning();
+    }
+
     // START GAME
     public void StartGame()
     {
-        SceneManager.LoadScene("GameScene"); // Let you complete
+        gameManager.bgmSource.Play();
+        playPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+
+        // Start gameplay systems
+        itemSpawner.StartSpawning();
+        weekTimer.StartNewWeek();
+    }
+
+    void OpenMainMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        playPanel.SetActive(false);
+        howToPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
     }
 
     // HOW TO PLAY
